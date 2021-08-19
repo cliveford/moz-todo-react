7d159e2acd5682b33a1994e11e9ffebd77ae1329
cliveford <78969930+cliveford@users.noreply.github.com>
2021-08-19 23:02:42 +0100

    added data array and components

--- src/App.js
+++ src/App.js
@@ -1,94 +1,33 @@
+// import components
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
+
 function App(props) {
+  // transform data
+  const taskList = props.tasks.map((task) => (
+    <Todo
+      id={task.todoid}
+      name={task.name}
+      completed={task.completed}
+      key={task.todoid}
+    />
+  ));
   return (
     <div className='todoapp stack-large'>
       <h1>You-To-Do</h1>
-      <form>
-        <h2 className='label-wrapper'>
-          <label htmlFor='new-todo-input' className='label__lg'>
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type='text'
-          id='new-todo-input'
-          className='input input__lg'
-          name='text'
-          autoComplete='off'
-        />
-        <button type='submit' className='btn btn__primary btn__lg'>
-          Add
-        </button>
-      </form>
+      <Form />
       <div className='filters btn-group stack-exception'>
-        <button type='button' className='btn toggle-btn' aria-pressed='true'>
-          <span className='visually-hidden'>Show </span>
-          <span>all</span>
-          <span className='visually-hidden'> tasks</span>
-        </button>
-        <button type='button' className='btn toggle-btn' aria-pressed='false'>
-          <span className='visually-hidden'>Show </span>
-          <span>Active</span>
-          <span className='visually-hidden'> tasks</span>
-        </button>
-        <button type='button' className='btn toggle-btn' aria-pressed='false'>
-          <span className='visually-hidden'>Show </span>
-          <span>Completed</span>
-          <span className='visually-hidden'> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
       <h2 id='list-heading'>3 tasks remaining</h2>
       <ul
         className='todo-list stack-large stack-exception'
         aria-labelledby='list-heading'
       >
-        <li className='todo stack-small'>
-          <div className='c-cb'>
-            <input id='todo-0' type='checkbox' defaultChecked={true} />
-            <label className='todo-label' htmlFor='todo-0'>
-              Eat
-            </label>
-          </div>
-          <div className='btn-group'>
-            <button type='button' className='btn'>
-              Edit <span className='visually-hidden'>Eat</span>
-            </button>
-            <button type='button' className='btn btn__danger'>
-              Delete <span className='visually-hidden'>Eat</span>
-            </button>
-          </div>
-        </li>
-        <li className='todo stack-small'>
-          <div className='c-cb'>
-            <input id='todo-1' type='checkbox' />
-            <label className='todo-label' htmlFor='todo-1'>
-              Sleep
-            </label>
-          </div>
-          <div className='btn-group'>
-            <button type='button' className='btn'>
-              Edit <span className='visually-hidden'>Sleep</span>
-            </button>
-            <button type='button' className='btn btn__danger'>
-              Delete <span className='visually-hidden'>Sleep</span>
-            </button>
-          </div>
-        </li>
-        <li className='todo stack-small'>
-          <div className='c-cb'>
-            <input id='todo-2' type='checkbox' />
-            <label className='todo-label' htmlFor='todo-2'>
-              Repeat
-            </label>
-          </div>
-          <div className='btn-group'>
-            <button type='button' className='btn'>
-              Edit <span className='visually-hidden'>Repeat</span>
-            </button>
-            <button type='button' className='btn btn__danger'>
-              Delete <span className='visually-hidden'>Repeat</span>
-            </button>
-          </div>
-        </li>
+        {taskList}
       </ul>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// create object array
+const DATA = [
+  { todoid: 'todo-0', name: 'Eat', completed: true },
+  { todoid: 'todo-1', name: 'Sleep', completed: false },
+  { todoid: 'todo-2', name: 'Repeat', completed: false },
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App subject='Clarice' />
+    <App tasks={DATA} />
   </React.StrictMode>,
   document.getElementById('root')
 );
